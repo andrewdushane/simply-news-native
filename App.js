f50192c53e832485363f25c370022033d388a82d
Andrew Dushane <andrewdushane@gmail.com>
2017-11-16 16:08:33 -0500

    Log axios requests

--- App.js
+++ App.js
@@ -1,4 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import Navigator from './components/Navigator';
+
+axios.interceptors.request.use(request => {
+  console.log('Starting Request', request);
+  return request;
+});
+
+axios.interceptors.response.use(response => {
+  console.log('Response:', response);
+  return response;
+});
 
 export default () => <Navigator />;
